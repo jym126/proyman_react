12799edc85db11845d1e0d70b775159e9765fd8a
jym126 <jym126@hotmail.com>
2024-11-09 22:59:51 +0100

    build4

--- src/App.js
+++ src/App.js
@@ -130,6 +130,11 @@ const App = () => {
 
   return (
     <div>
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Document</title>
+</head>
       <div className="header">
         <input
           type="text"
